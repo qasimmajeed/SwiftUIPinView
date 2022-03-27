d95defee4436a2c8eed02f3ed1ae5a01bb2e960c
Qasim Majeed <qm@next-munich.com>
2022-03-27 23:43:27 +0200

    initial commit XCode swiftUI project is created for PinView

---
+++ SwiftUIPinView/SwiftUIPinView/SwiftUIPinViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPinViewApp.swift
+//  SwiftUIPinView
+//
+//  Created by Qasim Majeed on 27.03.22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPinViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
